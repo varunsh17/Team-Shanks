11e7c564a0376641d0f714a6c64358395013cb64
varunsh17 <varunsh170502@gmail.com>
2021-06-27 12:52:15 +0530

    login page is responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React from 'react';
+import './login.css';
+import imageo from "./img/vr.jpg"
+import video1 from "./img/cosmic.mp4"
+import triangle from "./img/triangle.png"
 function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+
+		return (
+<div className="Login">
+
+<div className="all">
+    <video   muted autoPlay loop>
+  <source  src={video1} type="video/mp4"/></video>
+
+  <img className="triangle" src={triangle} alt=""/>
+
+  <a href="/"><div className="textp">PLAY IN VR</div></a>
+
+
+<img className="imgvr" src={imageo} alt=""/>
+
+</div>
+</div>
+	);
 }
 
-export default Login
+export default Login;
